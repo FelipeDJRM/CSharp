5842c54382b92b5dc980a0b2feee9662b10fadbf
kjc j k <kjc j k@DESKTOP-021FC4M>
2017-08-02 13:36:12 -0700

    Listo

---
+++ CSharp/CSharp/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            aludo obj = new aludo();
+            obj.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IMAGEN OBJ = new IMAGEN();
+            OBJ.Show();
+        }
+    }
+}
